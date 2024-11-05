a3557d00e11f1cb620ba85acfb40eacdaae96b8c
Camejoh <Cameron.johnnesha@gmail.com>
2024-11-05 17:47:10 -0600

    Create Cameron_HW09_2.cpp

---
+++ Cameron_HW09_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string filename;
+    cout << "Enter the filename: ";
+    cin >> filename;
+
+    ifstream inFile(filename);
+    if (!inFile) {
+        cout << "Error: Could not open file." << endl;
+        return 1;
+    }
+
+    string line;
+    int lineNumber = 1;
+    while (getline(inFile, line)) {
+        cout << setw(3) << lineNumber << ": " << line << endl;
+        lineNumber++;
+    }
+
+    inFile.close();
+    return 0;
+}
