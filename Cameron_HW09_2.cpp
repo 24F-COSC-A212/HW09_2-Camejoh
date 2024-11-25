dd3e9e7c580a2760fcdd31c6833ebf8f250e128b
Scott McDermott <smcdermo@loyno.edu>
2024-11-25 14:06:31 -0600

    Graded version

--- Cameron_HW09_2.cpp
+++ Cameron_HW09_2.cpp
@@ -3,22 +3,29 @@
 #include <iomanip>
 #include <string>
 
+// missing header area!
+// formatting errors!
+// missing right align
+
 using namespace std;
 
-int main() {
+int main()
+{
     string filename;
     cout << "Enter the filename: ";
     cin >> filename;
 
     ifstream inFile(filename);
-    if (!inFile) {
+    if (!inFile)
+    {
         cout << "Error: Could not open file." << endl;
         return 1;
     }
 
     string line;
     int lineNumber = 1;
-    while (getline(inFile, line)) {
+    while (getline(inFile, line))
+    {
         cout << setw(3) << lineNumber << ": " << line << endl;
         lineNumber++;
     }
